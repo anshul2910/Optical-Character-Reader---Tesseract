a914062a53352b5da0c9e7a55d8d4601b9d8f18e
Anshul Chaudhary <2910anshul1997@gmail.com>
2019-10-01 07:47:34 +0530

    Initial Commit

---
+++ OCR.py
@@ -0,0 +1,26 @@
+import os
+import tempfile
+import subprocess
+import pytesseract
+
+#pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+
+def ocr(path):
+    temp = tempfile.NamedTemporaryFile(delete=False)
+
+    process = subprocess.Popen(['tesseract', path, temp.name], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    process.communicate()
+
+    with open(temp.name + '.txt', 'r') as handle:
+        contents = handle.read()
+
+    os.remove(temp.name + '.txt')
+
+    #os.remove(temp.name)
+
+
+    return contents
+
+
+str = ocr('#your image path over here!')
+print(str)
